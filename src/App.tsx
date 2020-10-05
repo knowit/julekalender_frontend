09d7baef33727b8ce7979584547e2512b0290a4e
Sverre Johann Bjørke <sverrejb@gmail.com>
2020-10-05 16:49:37 +0200

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Door from './components/door';
+import Modal from 'react-modal';
+import Task from './components/task'
+
 
 function App() {
+
+  Modal.setAppElement('#root');
+
+  const [selectedDoor, selectDoor] = useState(0);
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+
+
+  function openDoor(doorNumber: number) {
+    selectDoor(doorNumber)
+    setIsOpen(true);
+  }
+
+  function closeModal(){
+    setIsOpen(false);
+  }
+
+ 
+
+  const customStyles = {
+    content : {
+      top                   : '50%',
+      left                  : '50%',
+      right                 : 'auto',
+      bottom                : 'auto',
+      marginRight           : '-50%',
+      transform             : 'translate(-50%, -50%)'
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <nav>KNOWIT-LOGO <button>LEDERTAVLE</button><button>LOGG INN</button></nav>
+        <h1>Knowit kodekalender 2020</h1>
       </header>
-    </div>
+      <main>
+        {[...Array(24)].map((x, i) =>
+          <Door onClick={() => openDoor(i+1)} key={i} number={i + 1} />
+        )}
+        {selectedDoor}
+        <Modal
+          isOpen={modalIsOpen}
+          style={customStyles}
+          contentLabel="Task modal"
+          onRequestClose={closeModal}
+        >
+          <h2>Luke {selectedDoor}</h2>
+          <button onClick={() => closeModal()}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </Modal>
+        
+      </main>
+      <footer>
+        <p>Kodekalenderen er kalenderen for deg som er glad i programmering. Hver luke er en oppgave som løses best ved hjelp av kode.</p>
+        <p>Lukene varierer i vanskelighetsgrad og utforming, men felles for alle er at koden til løsningen din skal resultere i et svar på en linje som systemet kan sjekke om er korrekt.</p>
+        <p>Hver luke som løses er et lodd i trekningen av en telefon eller et nettbrett. Løs så mange luker som mulig for å øke vinnersjansene dine!</p>
+        <p>Lukene åpnes klokken 06:00 hver dag, og du har 24 timer på på å løse oppgaven. Vinneren trekkes på nyåret og vil bli kontaktet. Lykke til og god jul!</p>
+        <a href="https://www.knowit.no/karriere/systemutvikler-backend-mobil-ogeller-frontend/">Vil du jobbe med oss?</a>
+      </footer>
+    </>
   );
 }
 

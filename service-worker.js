57b17c83095ecad9659a0daf711406be59a64a0f
sverrejb <sverrejb@gmail.com>
2020-10-06 20:29:55 +0000

    Deploying to gh-pages from  @ c4017af38be51db7a97adb592fade7b980a8e8b2 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/julekalender_frontend/precache-manifest.db8de8195e2806aae24b418adbc0c27e.js"
+  "/julekalender_frontend/precache-manifest.0280e7e32aae75897d5a0280b26b3177.js"
 );
 
 self.addEventListener('message', (event) => {

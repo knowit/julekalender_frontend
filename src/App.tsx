447dd4aa90d877bce2ecc8a5a156309b2cf5a677
Sverre Johann Bjørke <sverrejb@gmail.com>
2020-10-07 23:38:49 +0200

    god natt

--- src/App.tsx
+++ src/App.tsx
@@ -35,11 +35,15 @@ function App() {
         
       </main>
       <footer>
+        <p>KNOWIT-LOGO HER</p>
+        <div>SOCIAL MEDIA LINKS HER</div>
+        <h2>Om kodekalenderen</h2>
         <p>Kodekalenderen er kalenderen for deg som er glad i programmering. Hver luke er en oppgave som løses best ved hjelp av kode.</p>
         <p>Lukene varierer i vanskelighetsgrad og utforming, men felles for alle er at koden til løsningen din skal resultere i et svar på en linje som systemet kan sjekke om er korrekt.</p>
         <p>Hver luke som løses er et lodd i trekningen av en telefon eller et nettbrett. Løs så mange luker som mulig for å øke vinnersjansene dine!</p>
         <p>Lukene åpnes klokken 06:00 hver dag, og du har 24 timer på på å løse oppgaven. Vinneren trekkes på nyåret og vil bli kontaktet. Lykke til og god jul!</p>
-        <a href="https://www.knowit.no/karriere/systemutvikler-backend-mobil-ogeller-frontend/">Vil du jobbe med oss?</a>
+        <h2>Jobbe i Knowit?</h2>
+        <p>Blablabla søk jobb</p>
       </footer>
     </>
   );

2f3ed6646f0a8aaf5fdc6aedc3659aaac731c873
Sverre Johann Bjørke <sverrejb@gmail.com>
2020-10-18 22:50:04 +0200

    started on login-stuff

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Test from './components/test'
 import Task from './components/task';
 import Footer from './components/Footer';
 import StarBackground from './effects/stars'
+import LoginButton from './components/LoginButton';
+
 
 
 
@@ -22,11 +24,12 @@ function App() {
     setIsOpen(false);
   }
 
+
   return (
     <>
       <StarBackground/>
       <header>
-        <nav><button>LEDERTAVLE</button><button>LOGG INN</button></nav>
+        <nav><button>LEDERTAVLE</button><LoginButton/></nav>
         <h1>Kodekalender 2020</h1>
         <p>Løs lukene og bli med i trekningen av en telefon eller et nettbrett!</p>
         <p>En ny luke åpnes hver dag frem til jul.</p>
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="knowit-julekalender.eu.auth0.com"
+    clientId="yAtCtCcXkAkwF4Crd7Cn9Wi10F0mVVc0"
+    redirectUri={window.location.origin}
+    audience="https://YOUR_DOMAIN/api/v2/"
+    scope="read:current_user update:current_user_metadata"
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById('root')
 );

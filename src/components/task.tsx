db48590466b1425a1eae48b1f3407a03c6548145
Sverre Johann Bjørke <sverrejb@gmail.com>
2020-10-19 23:40:44 +0200

    started on markdown rendering

--- src/components/task.tsx
+++ src/components/task.tsx
@@ -1,42 +1,45 @@
-import React, { CSSProperties } from 'react'
+import React from 'react'
 import './Task.css';
 import Modal from 'react-modal';
+import ReactMarkdown from 'react-markdown'
 
 type TaskProps = {
-     number: number,
-     modalIsOpen: boolean,
-     closeHandler: () => void
-}; 
+  number: number,
+  modalIsOpen: boolean,
+  closeHandler: () => void
+};
 
 
 const Task = ({ number, modalIsOpen, closeHandler }: TaskProps) => {
 
   const customStyles = {
-          content : {
-            top                   : '50%',
-            left                  : '50%',
-            right                 : 'auto',
-            bottom                : 'auto',
-            marginRight           : '-50%',
-            transform             : 'translate(-50%, -50%)'
-          }
-        };
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)'
+    }
+  };
 
-     return <Modal
-     isOpen={modalIsOpen}
-     style={customStyles}
-     contentLabel="Task modal"
-     onRequestClose={closeHandler}
-     closeTimeoutMS={1500}
-   >
-     <h2>Luke {number}</h2>
-     <button onClick={() => closeHandler()}>close</button>
-     <div>1 + 1 = ?</div>
-     <form>
-       <input />
-     </form>
-   </Modal>
-}     
+  const markdown = `# Heading  
+  Lorem ipsum dolor sit amet. Foo bar bacon chips crisp.`
+
+  return <Modal
+    isOpen={modalIsOpen}
+    style={customStyles}
+    contentLabel="Task modal"
+    onRequestClose={closeHandler}
+    closeTimeoutMS={1500}
+  >
+    <button onClick={() => closeHandler()}>close</button>
+    <ReactMarkdown>{markdown}</ReactMarkdown>
+    <form>
+      <input />
+    </form>
+  </Modal>
+}
 
 
 export default Task;

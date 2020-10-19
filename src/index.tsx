24bcd81a3f919c05f880f9ead050e747a7d576be
Sverre Johann Bjørke <sverrejb@gmail.com>
2020-10-19 23:32:58 +0200

    commented out audience

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ ReactDOM.render(
     domain="knowit-julekalender.eu.auth0.com"
     clientId="yAtCtCcXkAkwF4Crd7Cn9Wi10F0mVVc0"
     redirectUri={window.location.href} //Denne må kanskje endrast?
-    audience="https://YOUR_DOMAIN/api/v2/"
+    //audience="https://YOUR_DOMAIN/api/v2/" TODO HOST på APIET
     scope="read:current_user update:current_user_metadata" //TODO: Lag scopes i backend? Sjekk opp
   >
     <App />

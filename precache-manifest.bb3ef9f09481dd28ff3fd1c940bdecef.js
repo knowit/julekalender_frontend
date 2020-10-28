d0a06323656928debfb7b00ba1dd9c48eeeca660
sverrejb <sverrejb@gmail.com>
2020-10-28 22:42:35 +0000

    Deploying to gh-pages from  @ 44d62a40751e981beec8647720c67b823ddd9f55 🚀

---
+++ precache-manifest.bb3ef9f09481dd28ff3fd1c940bdecef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7327369a5a1a9113d804d5b7d45504f4",
+    "url": "/julekalender_frontend/index.html"
+  },
+  {
+    "revision": "d8afa6a9f4d8f931fe60",
+    "url": "/julekalender_frontend/static/css/main.2b7a69b7.chunk.css"
+  },
+  {
+    "revision": "40e42223b2331ae06d04",
+    "url": "/julekalender_frontend/static/js/2.08212d48.chunk.js"
+  },
+  {
+    "revision": "f11637696e9dc30bb77c67bc7e5f2f9d",
+    "url": "/julekalender_frontend/static/js/2.08212d48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8afa6a9f4d8f931fe60",
+    "url": "/julekalender_frontend/static/js/main.7be24f50.chunk.js"
+  },
+  {
+    "revision": "537a51ebde584d8b138a",
+    "url": "/julekalender_frontend/static/js/runtime-main.a76bf04a.js"
+  },
+  {
+    "revision": "a76d39e363a828cbdcd47ced2c7e1ed5",
+    "url": "/julekalender_frontend/static/media/background.a76d39e3.svg"
+  },
+  {
+    "revision": "cd728b8b01eab898312f360d3689af2b",
+    "url": "/julekalender_frontend/static/media/foreground.cd728b8b.svg"
+  },
+  {
+    "revision": "717cba502d44cd58a641ea0733639b63",
+    "url": "/julekalender_frontend/static/media/knowitlogo.717cba50.svg"
+  },
+  {
+    "revision": "770daafaa33f3b19b165a35563a80104",
+    "url": "/julekalender_frontend/static/media/midground.770daafa.svg"
+  }
+]);

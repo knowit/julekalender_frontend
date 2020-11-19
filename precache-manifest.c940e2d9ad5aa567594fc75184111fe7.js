5960978af0c60732af6b18d04fd966c22e120ab1
sverrejb <sverrejb@gmail.com>
2020-11-19 18:36:13 +0000

    Deploying to gh-pages from  @ ab2506175e153f8a24d165319b5554e053015156 🚀

---
+++ precache-manifest.c940e2d9ad5aa567594fc75184111fe7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9754d44ee7ea2d4491889fe1176de287",
+    "url": "/julekalender.knowit.no/index.html"
+  },
+  {
+    "revision": "6716db9dbf8904116148",
+    "url": "/julekalender.knowit.no/static/css/main.5fd038b9.chunk.css"
+  },
+  {
+    "revision": "ecc8b041f84b3e4fc5cb",
+    "url": "/julekalender.knowit.no/static/js/2.ecf64218.chunk.js"
+  },
+  {
+    "revision": "f11637696e9dc30bb77c67bc7e5f2f9d",
+    "url": "/julekalender.knowit.no/static/js/2.ecf64218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6716db9dbf8904116148",
+    "url": "/julekalender.knowit.no/static/js/main.ae85d791.chunk.js"
+  },
+  {
+    "revision": "c7a42acc296307dea9bd",
+    "url": "/julekalender.knowit.no/static/js/runtime-main.2d8e81b4.js"
+  },
+  {
+    "revision": "a76d39e363a828cbdcd47ced2c7e1ed5",
+    "url": "/julekalender.knowit.no/static/media/background.a76d39e3.svg"
+  },
+  {
+    "revision": "cd728b8b01eab898312f360d3689af2b",
+    "url": "/julekalender.knowit.no/static/media/foreground.cd728b8b.svg"
+  },
+  {
+    "revision": "717cba502d44cd58a641ea0733639b63",
+    "url": "/julekalender.knowit.no/static/media/knowitlogo.717cba50.svg"
+  },
+  {
+    "revision": "770daafaa33f3b19b165a35563a80104",
+    "url": "/julekalender.knowit.no/static/media/midground.770daafa.svg"
+  },
+  {
+    "revision": "c2dd0cd4af53d77a8d159676f11563ba",
+    "url": "/julekalender.knowit.no/static/media/mistletoeborder.c2dd0cd4.svg"
+  },
+  {
+    "revision": "4348a70a4433a28d9e767b95084fe768",
+    "url": "/julekalender.knowit.no/static/media/pointsdecor.4348a70a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7514168f0c5d8da642d53cc327d70231.js"
+  "/julekalender.knowit.no/precache-manifest.c940e2d9ad5aa567594fc75184111fe7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/julekalender.knowit.no/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

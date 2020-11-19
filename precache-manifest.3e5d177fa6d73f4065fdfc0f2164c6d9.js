9e8c9ce82f7f7b658052c350fc833fc4d025e9e6
sverrejb <sverrejb@gmail.com>
2020-11-19 07:33:30 +0000

    Deploying to gh-pages from  @ 6fe8bb9bd4952f953cfcc6adf6d5255384084d87 🚀

---
+++ precache-manifest.3e5d177fa6d73f4065fdfc0f2164c6d9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68b47165c318049831c30840205f1ef7",
+    "url": "/julekalender_frontend/index.html"
+  },
+  {
+    "revision": "186c47ac403e002b5996",
+    "url": "/julekalender_frontend/static/css/main.bab001e0.chunk.css"
+  },
+  {
+    "revision": "6d4558539c8b7a015f1a",
+    "url": "/julekalender_frontend/static/js/2.6e9279d2.chunk.js"
+  },
+  {
+    "revision": "f11637696e9dc30bb77c67bc7e5f2f9d",
+    "url": "/julekalender_frontend/static/js/2.6e9279d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "186c47ac403e002b5996",
+    "url": "/julekalender_frontend/static/js/main.0b22c271.chunk.js"
+  },
+  {
+    "revision": "537a51ebde584d8b138a",
+    "url": "/julekalender_frontend/static/js/runtime-main.a76bf04a.js"
+  },
+  {
+    "revision": "a76d39e363a828cbdcd47ced2c7e1ed5",
+    "url": "/julekalender_frontend/static/media/background.a76d39e3.svg"
+  },
+  {
+    "revision": "cd728b8b01eab898312f360d3689af2b",
+    "url": "/julekalender_frontend/static/media/foreground.cd728b8b.svg"
+  },
+  {
+    "revision": "717cba502d44cd58a641ea0733639b63",
+    "url": "/julekalender_frontend/static/media/knowitlogo.717cba50.svg"
+  },
+  {
+    "revision": "770daafaa33f3b19b165a35563a80104",
+    "url": "/julekalender_frontend/static/media/midground.770daafa.svg"
+  },
+  {
+    "revision": "4348a70a4433a28d9e767b95084fe768",
+    "url": "/julekalender_frontend/static/media/pointsdecor.4348a70a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/julekalender_frontend/precache-manifest.5c4084f3a3e2bc6a85a73f0da595478b.js"
+  "/julekalender_frontend/precache-manifest.3e5d177fa6d73f4065fdfc0f2164c6d9.js"
 );
 
 self.addEventListener('message', (event) => {

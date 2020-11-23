45703e752f96d875b4302b6fc13218505977369d
Sverre Johann Bjørke <sverrejb@gmail.com>
2020-11-23 22:52:49 +0100

    comments stuff, new server IP

--- src/components/LeaderBoard.tsx
+++ src/components/LeaderBoard.tsx
@@ -7,182 +7,49 @@ type LeaderBoardProps = {
     open: boolean
 };
 
-const LeaderBoard: FC<LeaderBoardProps> = ({closeHandler, open}) => {
+const LeaderBoard: FC<LeaderBoardProps> = ({ closeHandler, open }) => {
     return <aside className={`Leaderboard ${open ? 'open' : ''}`}>
-    <button title="Lukk ledertavle" id="CloseLeaderboard" onClick={() => closeHandler()}>
-      {/*X-icon svg*/}
-      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18"><path fill="#fff" d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"/></svg>
-      </button>
-    <h2>SNILLE BARN</h2>
-    <Flourish className="flourish"/>
-    <table>
-        <thead>
-        <tr>
-            <th>LUKER LØST</th>
-            <th>BRUKERNAVN</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        <tr>
-            <td>24</td>
-            <td>Sverre</td>
-        </tr>
-        
-        
-        </tbody>
-    </table>
+        <button title="Lukk ledertavle" id="CloseLeaderboard" onClick={() => closeHandler()}>
+            {/*X-icon svg*/}
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18"><path fill="#fff" d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z" /></svg>
+        </button>
+        <h2>SNILLE BARN</h2>
+        <Flourish className="flourish" />
+        <table>
+            <thead>
+                <tr>
+                    <th>LUKER LØST</th>
+                    <th>BRUKERNAVN</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>24</td>
+                    <td>
+                        <ul>
+                            <li>Per</li>
+                            <li>Kari</li>
+                            <li>Per</li>
+                            <li>Kari</li>
+                            <li>Per</li>
+                            <li>Kari</li>
+                            <li>PerPerPerPerPerPerPerPer</li>
+                            <li>Kari</li>
+                            <li>Per</li>
+                            <li>Kari</li>
+                        </ul></td>
+                </tr>
+                <tr>
+                    <td>23</td>
+                    <td>
+                        <ul>
+                            <li>Ole</li>
+                            <li>Siri</li>
+                            <li>Donald</li>
+                        </ul></td>
+                </tr>
+            </tbody>
+        </table>
     </aside>;
 }
 

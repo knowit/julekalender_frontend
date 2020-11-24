30694e34def3e2493a92ce6308a1daa9e4841333
vebovs <vebovs@hotmail.no>
2020-11-24 13:38:17 +0100

    gdpr side

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import Footer from './components/Footer';
 import StarBackground from './effects/stars'
 import LoginButton from './components/LoginButton';
 import LeaderBoard from './components/LeaderBoard';
+import Gdpr from './components/Gdpr';
 import { ReactComponent as Logo } from './img/knowitlogo.svg';
 import {
   Switch,
   Route,
   Redirect,
+  Link,
 } from "react-router-dom";
 import Door from './components/Door';
 
@@ -30,6 +32,7 @@ function App() {
           <header>
             <nav>
               <a id="knowitlogo" href="https://www.knowit.no/" target="_blank" rel="noopener noreferrer" tabIndex={1}><Logo /></a>
+              <Link id='gdprLink' to='/gdpr' title='gdpr'>GDPR</Link>
               <button onClick={() => toggleLeaderBoard(!leaderBoardOpen)} tabIndex={2}>LEDERTAVLE</button>
               <LoginButton />
             </nav>
@@ -42,6 +45,9 @@ function App() {
             {/* Match door 1-24 only*/}
             <Route path="/luke/:doorNumber(0?[1-9]|1[0-9]|2[0-4])">
               <Door />
+            </Route>
+            <Route path='/gdpr'>
+              <Gdpr />
             </Route>
             {/* 404? - Route to main view*/}
             <Route>

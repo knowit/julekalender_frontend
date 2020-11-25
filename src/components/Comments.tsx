daed97aebe07f30269b33fbb2e8a36af452cc8bd
vebovs <vebovs@hotmail.no>
2020-11-25 11:38:14 +0100

    first itieration of commen interaction footer

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,9 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import './Comments.css';
 import Comment from '../api/Comment';
 import TextareaAutosize from 'react-autosize-textarea/lib';
-
-
+import { ReactComponent as Favorite } from './svg/favorite.svg';
+import { ReactComponent as Chevron } from './svg/expand_more.svg';
 
 const Comments = () => {
     const dummyData: Comment[] = [{ content: "Foo bar baz", likes: 3 }, { content: "Foo bar baz", likes: 3 }, { content: "Foo bar baz", likes: 3 }]
@@ -18,21 +18,50 @@ interface CommentProps {
 }
 
 const CommentView: FC<CommentProps> = ({ comment }) => {
+    const [replies, setReplies] = useState(false) // Hide/Show replies
+
+    // Dummy Data
+    const data = {
+        liked: false,
+        likes: 12,
+        repliesNumber: 2
+    }
+    const liked: boolean = true
+
+    const toggleReplies = () => {
+        setReplies(!replies)
+    }
 
     const subComments: Comment[] = [{ content: "Foo bar baz", likes: 3 }, { content: "Foo bar baz", likes: 3 }, { content: "Foo bar baz", likes: 3 }]
 
-    return <div className="Comment"><div className="CommentView">
-        <img className="ProfileImage" src="https://placekitten.com/100/100" alt="User avatar" />
-        <div className="CommentData">
-            <span className="CommentName">Name</span><time>12. des 14:27</time>
-            <p>{comment.content}</p>
+    return (
+        <div className="Comment">
+            <div className="CommentView">
+                <img className="ProfileImage" src="https://placekitten.com/100/100" alt="User avatar" />
+                <div className="CommentData">
+                    <span className="CommentName">Name</span><time>12. des 14:27</time>
+                    <p>{comment.content}</p>
+                </div>
+            </div>
+            <div className='CommentFooter'>
+                <div className='CommentFooterItem LikeWrapper'>
+                    <Favorite className={data.liked ? 'favoriteSvgLiked' : 'favoriteSvg'}/>
+                    <p>{data.likes}</p>
+                </div>
+                <h4 className='CommentFooterItem'>SVAR</h4>
+                <button className='CommentFooterItem btnReplies' onClick={toggleReplies}>
+                    <p>vis {data.repliesNumber} svar </p>
+                    <Chevron className={replies ? 'Chevron Rotate' : 'Chevron'}/>
+                </button>
+            </div>
+            { replies && 
+                <div className="SubComments">
+                    {subComments.map(comment => <SubComment comment={comment} />)}
+                </div>
+            }
         </div>
-    </div>
-        <div className="SubComments">
-            {subComments.map(comment => <SubComment comment={comment} />)}
-        </div>
-    </div>
-}
+    )
+}  
 
 
 const SubComment: FC<CommentProps> = ({ comment }) => {

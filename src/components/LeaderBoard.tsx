a0e631095797fd5e5d21da2322b8cad71eb9a5d3
Sverre Johann Bjørke <sverrejb@gmail.com>
2020-11-26 23:06:59 +0100

    fix leaderboard name display

--- src/components/LeaderBoard.tsx
+++ src/components/LeaderBoard.tsx
@@ -45,8 +45,8 @@ const LeaderBoard: FC<LeaderBoardProps> = ({ closeHandler, open }) => {
                 </tr>
             </thead>
             <tbody>
-                {Object.keys(lb)
-                    .map((k) => <tr key="k"><td>{k}</td><td>{lb[k as DoorKey]}</td></tr>)}
+                {Object.keys(lb).reverse()
+                    .map((k) => <tr key="k"><td>{k}</td><td>{lb[k as DoorKey]?.map(name => <p>{name}</p>)}</td></tr>)}
             </tbody>
         </table>
     </aside>;

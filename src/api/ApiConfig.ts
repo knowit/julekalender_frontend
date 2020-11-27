ce8658debdce3883b6b9f3c3cecf51e521600d0c
Sverre Johann Bjørke <sverrejb@gmail.com>
2020-11-27 19:04:41 +0100

    add file

---
+++ src/api/ApiConfig.ts
@@ -0,0 +1,4 @@
+export const apiUrl = 'https://julekalender-backend-dev.knowit.no';
+export const requestOptions = { headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" } }
+
+

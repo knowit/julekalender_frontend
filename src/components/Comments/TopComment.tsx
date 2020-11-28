bb1653d4599de70713a274b39e32a830cadd954b
Sverre Johann Bjørke <sverrejb@gmail.com>
2020-11-28 17:08:08 +0100

    css jukking på kommentarfelt

--- src/components/Comments/TopComment.tsx
+++ src/components/Comments/TopComment.tsx
@@ -6,33 +6,11 @@ import SubComment from './SubComment';
 import ParentComment from '../../api/Comment';
 
 
-
-
 interface CommentProps {
     comment: ParentComment;
 }
 
 const TopComment: FC<CommentProps> = ({ comment }) => {
-    const [replies, setReplies] = useState(false) // Hide/Show replies
-    const [reply, setReply] = useState(false) // Hide/Show reply
-    const [displayReplies, setDisplayReplies] = useState(true) // Hide/Show the show replies button when the reply section is open
-
-    // Placeholder data
-    const data = {
-        liked: false,
-        likes: 12,
-        repliesNumber: 2
-    }
-
-    const toggleReplies = () => {
-        setReplies(replies)
-    }
-
-    const toggleReply = () => {
-        setReply(!reply)
-        setDisplayReplies(reply ? true : false)
-        setReplies(reply && replies ? true : false)
-    }
 
     return (
         <div className="Comment">
@@ -45,34 +23,34 @@ const TopComment: FC<CommentProps> = ({ comment }) => {
             </div>
             <div className='CommentFooter'>
                 <div className='CommentFooterItem LikeWrapper'>
-                    <Favorite className={data.liked ? 'favoriteSvgLiked' : 'favoriteSvg'} />
-                    <p>{data.likes}</p>
+                    <Favorite className={true ? 'favoriteSvgLiked' : 'favoriteSvg'} />
+                    <p>{comment.likes}</p>
                 </div>
-                <button className='CommentFooterItem btnReply' onClick={toggleReply}>SVAR</button>
-                {displayReplies &&
-                    <button className='CommentFooterItem btnReplies' onClick={toggleReplies}>
-                        <p>vis {data.repliesNumber} svar </p>
-                        <Chevron className={replies ? 'Chevron Rotate' : 'Chevron'} />
-                    </button>
-                }
+                <button className='CommentFooterItem btnReply' onClick={() => alert("fooo")}>SVAR</button>
+
+                <button className='CommentFooterItem btnReplies' onClick={() => alert("asdasd")}>
+                    <p>Vis {comment.children?.length} svar</p>
+                    <Chevron className={true ? 'Chevron Rotate' : 'Chevron'} />
+                </button>
+
             </div>
-            { reply &&
-                <div className='ReplyBox'>
-                    <div className='ReplyBoxInput'>
-                        <img className="ProfileImage" src="https://placekitten.com/100/100" alt="User avatar" />
-                        <TextareaAutosize id='ReplyText' placeholder='Legg til svar' />
-                    </div>
-                    <div className='ReplyBoxButtons'>
-                        <button className='ReplyBoxBtn' onClick={toggleReply}>AVBRYT</button>
-                        <button className='ReplyBoxBtn'>SVAR</button>
-                    </div>
+
+            {false ? <div className='ReplyBox'>
+                <div className='ReplyBoxInput'>
+                    <img className="ProfileImage" src="https://placekitten.com/100/100" alt="User avatar" />
+                    <TextareaAutosize id='ReplyText' placeholder='Legg til svar' />
                 </div>
-            }
-            { replies &&
-                <div className="SubComments">
-                    {comment.children?.map(comment => <SubComment comment={comment} />)}
+                <div className='ReplyBoxButtons'>
+                    <button className='ReplyBoxBtn' onClick={() => alert("woolp")}>AVBRYT</button>
+                    <button className='ReplyBoxBtn'>SVAR</button>
                 </div>
-            }
+            </div> : null}
+
+
+            <div className="SubComments">
+                {comment.children?.map(comment => <SubComment comment={comment} />)}
+            </div>
+
         </div>
     )
 }

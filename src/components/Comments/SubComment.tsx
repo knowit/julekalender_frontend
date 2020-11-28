9942f9970a68b735d49dc51f6610c02f90da5753
Sverre Johann Bjørke <sverrejb@gmail.com>
2020-11-28 21:56:08 +0100

    remove placeholder data in subcomment

--- src/components/Comments/SubComment.tsx
+++ src/components/Comments/SubComment.tsx
@@ -9,12 +9,6 @@ interface SubCommentProps {
 
 const SubComment: FC<SubCommentProps> = ({ comment }) => {
 
-    // Placeholder data
-    const data = {
-        liked: false,
-        likes: 2
-    }
-
     return (
         <div className="SubComment">
             <img className="ProfileImage" src="https://placekitten.com/100/100" alt="User avatar" />
@@ -22,8 +16,8 @@ const SubComment: FC<SubCommentProps> = ({ comment }) => {
                 <span className="CommentName">Name</span><time>12. des 14:27</time>
                 <p>{comment.content}</p>
                 <div className='LikeWrapper'>
-                    <Favorite className={data.liked ? 'favoriteSvgLiked' : 'favoriteSvg'} />
-                    <p>{data.likes}</p>
+                    <Favorite className={comment.liked_by_me ? 'favoriteSvgLiked' : 'favoriteSvg'} />
+                    <p>{comment.likes}</p>
                 </div>
             </div>
         </div>

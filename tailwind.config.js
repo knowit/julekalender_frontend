20e278de1eba9fff1cccb5b6a7a7fe514ac812a6
Hugo Wallenburg <hugow@hey.com>
2020-11-29 16:52:58 +0100

    Adds Tailwind styling for Doors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lightbulb-green': 'rgb(108, 171, 133)',
+        'lightbulb-dim': 'rgba(224, 185, 88, 0.5)',
+        'lightbulb-yellow': 'rgb(224, 185, 88)',
+      },
+    },
   },
   variants: {
     extend: {},

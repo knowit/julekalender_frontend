18e5c92327d327817aa4cddb1eb39d3974cef0c5
Sverre Johann Bjørke <sverrejb@gmail.com>
2020-11-29 13:31:48 +0100

    Update Doors.tsx

--- src/components/Doors.tsx
+++ src/components/Doors.tsx
@@ -24,7 +24,7 @@ const Doors = () => {
 		if(new Date().getDate() >= doorNumber) {
 			return 'LightBulbOn'
 		}
-		return "LightBulbOff"
+		return "LightBulbDim"
 	}
 
 	const getLinkLocation = (doorNumber: number) => {

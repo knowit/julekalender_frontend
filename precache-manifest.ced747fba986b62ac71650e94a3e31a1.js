600dc5f615723a8af65231c36ebc6391a937628b
sverrejb <sverrejb@gmail.com>
2020-11-29 22:43:12 +0000

    Deploying to gh-pages from  @ 288afc6ce7938aaba1cf2438b82661002366ed69 🚀

---
+++ precache-manifest.ced747fba986b62ac71650e94a3e31a1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20b134a6cc26d1fcf946b5e69bbd18e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "709f4d154fb783a6da11",
+    "url": "/static/css/main.82a1cea5.chunk.css"
+  },
+  {
+    "revision": "287327a184f3f6e625ef",
+    "url": "/static/js/2.80ec7390.chunk.js"
+  },
+  {
+    "revision": "918e7d6c80ee1bb2948745150a8c95df",
+    "url": "/static/js/2.80ec7390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "709f4d154fb783a6da11",
+    "url": "/static/js/main.c1982e7a.chunk.js"
+  },
+  {
+    "revision": "24fc2a0b56bb461e0baf",
+    "url": "/static/js/runtime-main.3d352632.js"
+  },
+  {
+    "revision": "a76d39e363a828cbdcd47ced2c7e1ed5",
+    "url": "/static/media/background.a76d39e3.svg"
+  },
+  {
+    "revision": "ca822a5ed2ea6ab99054352d6e2a6764",
+    "url": "/static/media/expand_more.ca822a5e.svg"
+  },
+  {
+    "revision": "d9b06de9c4288d366ab61c0dc60b357f",
+    "url": "/static/media/favorite.d9b06de9.svg"
+  },
+  {
+    "revision": "cd728b8b01eab898312f360d3689af2b",
+    "url": "/static/media/foreground.cd728b8b.svg"
+  },
+  {
+    "revision": "863a3dad5ad465a5b94254a18887e75a",
+    "url": "/static/media/knowitlogo.863a3dad.svg"
+  },
+  {
+    "revision": "770daafaa33f3b19b165a35563a80104",
+    "url": "/static/media/midground.770daafa.svg"
+  },
+  {
+    "revision": "2505f3b48c0c442a05e56ffbbe4cc301",
+    "url": "/static/media/mistletoeborder.2505f3b4.svg"
+  },
+  {
+    "revision": "4348a70a4433a28d9e767b95084fe768",
+    "url": "/static/media/pointsdecor.4348a70a.svg"
+  }
+]);

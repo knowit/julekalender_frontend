1b4eb66bcf1ee5faaf0b48a57b690a681946d2a2
Hugo Wallenburg <hugow@hey.com>
2020-11-29 15:42:49 +0100

    Adds initial Tailwind config and some restyled components

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -3,11 +3,12 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = () => {
   const { loginWithRedirect, logout, isAuthenticated} = useAuth0();  
-  if(isAuthenticated){
-    return <button tabIndex={3} onClick={() => logout({ returnTo:window.location.origin })}>LOGG UT</button>
-  }
-  return <button tabIndex={3} onClick={() => loginWithRedirect()}>LOGG INN</button>
 
+  const [onClick, content] = isAuthenticated
+    ? [() => logout({ returnTo: window.location.origin }), 'Logg ut']
+    : [() => loginWithRedirect(), 'Logg inn']
+
+  return <button className="hover:underline uppercase" tabIndex={3} onClick={onClick}>{content}</button>
 };
 
 export default LoginButton

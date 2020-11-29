d47db04f004f2dbe498edc53445f2ece781ada84
Sverre Johann Bjørke <sverrejb@gmail.com>
2020-11-29 21:25:00 +0100

    Extract like button to component

--- src/components/Comments/SubComment.tsx
+++ src/components/Comments/SubComment.tsx
@@ -1,17 +1,30 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { ReactComponent as Favorite } from './../svg/favorite.svg';
 import { Comment } from '../../api/Comment';
 import Like from '../../api/Like';
 import { getTimeStamp } from '../../utils';
+import { useRequests } from '../../api/requests';
 
 
 interface SubCommentProps {
     comment: Comment;
-    likes: Like[];
+    myLikes: Like[];
 }
 
-const SubComment: FC<SubCommentProps> = ({ comment, likes }) => {
-    const isCommentLiked = likes.some((like) => like.post_uuid === comment.uuid)
+const SubComment: FC<SubCommentProps> = ({ comment, myLikes }) => {
+    var [numberOfLikes, setLikes] = useState<number>(comment.likes)
+    var [isCommentLiked, setIsCommentLiked] = useState<boolean>(myLikes.some((like) => like.post_uuid === comment.uuid))
+    const { createLike } = useRequests()
+
+
+    const likePost = () => {
+        createLike(comment.uuid)
+            .then(_ => {
+                setLikes(numberOfLikes + 1);
+                setIsCommentLiked(true);
+            })
+            .catch(e => {})
+    }
 
     return (
         <div className="SubComment">
@@ -19,10 +32,10 @@ const SubComment: FC<SubCommentProps> = ({ comment, likes }) => {
             <div className="CommentData">
                 <span className="CommentName">{comment.author.nickname}</span><time>{getTimeStamp(comment.created_at)}</time>
                 <div dangerouslySetInnerHTML={{ __html: comment.content }} />
-                <div className='LikeWrapper'>
+                <button className='LikeWrapper' onClick={() => likePost()}>
                     <Favorite className={isCommentLiked ? 'favoriteSvgLiked' : 'favoriteSvg'} />
                     <p>{comment.likes}</p>
-                </div>
+                </button>
             </div>
         </div>
     )

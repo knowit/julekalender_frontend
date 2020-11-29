11e88dc98489a7d0c9aa52ccd2203ef7c5155150
Hugo Wallenburg <hugow@hey.com>
2020-11-29 15:50:25 +0100

    Fixes Knowit logo styling inside door

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function App() {
           <header>
             <nav className="p-4">
               <a className="inline-block float-left" href="https://www.knowit.no/" target="_blank" rel="noopener noreferrer" tabIndex={1}>
-                <Logo className="h-7 md:h-10" />
+                <Logo className="h-7 md:h-10 fill-current" />
               </a>
               <div className="float-right space-x-4 h-10 text-md mt-0.5 md:mt-1 md:text-xl">
                 <button className="hover:underline uppercase" onClick={() => toggleLeaderBoard(!leaderBoardOpen)} tabIndex={2}>Ledertavle</button>

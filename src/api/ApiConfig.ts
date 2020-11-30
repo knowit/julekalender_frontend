8779e59902c07abad33e0c66bee9d90fac811117
Sverre Johann Bjørke <sverrejb@gmail.com>
2020-11-30 23:03:21 +0100

    submit on enter

--- src/api/ApiConfig.ts
+++
@@ -1,4 +0,0 @@
-export const apiUrl = window.location.hostname === 'julekalender.knowit.no'
-    ? 'https://julekalender-backend.knowit.no'
-    : 'https://julekalender-backend-dev.knowit.no';
-export const requestHeaders = { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" }

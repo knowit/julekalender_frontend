6c6fb098392be656d93ba1a9d69ea0e22e3ae8c4
Sverre Johann Bjørke <sverrejb@gmail.com>
2020-12-01 18:01:28 +0100

    fix typo in footer, filter out subcomment

--- src/components/Comments/CommentsSection.tsx
+++ src/components/Comments/CommentsSection.tsx
@@ -36,7 +36,7 @@ const CommentsSection: FC<CommentsSectionProps> = ({ doorNumber }) => {
     return (
         <section className="CommentSection">
             <CommentForm doorNumber={doorNumber} appendComment={appendComment} />
-            {comments.map((comment) => <TopComment key={comment.uuid} doorNumber={doorNumber} comment={comment} myLikes={likes} />)}
+            {comments.filter(m => m.children).map((comment) => <TopComment key={comment.uuid} doorNumber={doorNumber} comment={comment} myLikes={likes} />)}
         </section>
     )
 }

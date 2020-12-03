9f9bac29656604d0179825c67a0491d485e77d9b
Hugo Wallenburg <hugow@hey.com>
2020-12-03 22:20:00 +0100

    Adds user count to leaderboard headers

--- src/components/LeaderBoard.tsx
+++ src/components/LeaderBoard.tsx
@@ -58,7 +58,9 @@ const LeaderBoard: FC<LeaderBoardProps> = ({ hidden, setIsLeaderboardHiding, clo
           <div className="h-96 xl:h-192 overflow-y-auto">
             {leaderboard.map(([solved, users]) => (
               <div key={solved}>
-                <h3 className="sticky top-0 py-0.5 bg-lightbulb-green rounded-md text-lg text-center" key={solved}>{solved} luker løst</h3>
+                <h3 className="sticky top-0 py-1 bg-lightbulb-green rounded-md text-md tracking-wide text-center" key={solved} >
+                  <span className="font-semibold">{solved} luke{solved >= 1 && 'r'}</span> &mdash; <span className="text-gray-200 text-opacity-80">{users.length} snil{users.length > 1 ? 'le' : 't'} barn</span>
+                </h3>
                 <div className="pt-2 pb-4 space-y-1">
                   {users.map((user) => (
                     <p className="text-center" key={user}>{user}</p>

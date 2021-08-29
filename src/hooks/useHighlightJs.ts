9bf3acadb0ddd3b5987a79a4e89f6ffab2470e0f
Hugo Wallenburg <hugow@hey.com>
2021-08-29 18:08:04 +0200

    Dynamically loads highlight.js for a dramatically smaller bundle

--- src/hooks/useHighlightJs.ts
+++ src/hooks/useHighlightJs.ts
@@ -1,21 +1,22 @@
-import { useEffect, useRef } from "react"
-import hljs from "highlight.js"
+import { DependencyList, useEffect, useRef } from "react"
 import { forEach } from "lodash"
 
 
 // Sets syntax highlighting on all pre-blocks contained within the element given
 // by the returned ref. Must be given a HTMLElement-subtype matching that of the
 // elment you are attaching the ref to.
-const useHighlightJs = <T extends HTMLElement>() => {
+const useHighlightJs = <T extends HTMLElement>(dependencies: DependencyList = []) => {
   const hlRef = useRef<T>(null)
 
   useEffect(() => {
-    if (!hlRef.current) return
+    import("highlight.js").then((hljs) => {
+      if (!hlRef.current) return
 
-    forEach(hlRef.current.querySelectorAll<HTMLElement>("pre > code"), (block) => {
-      hljs.highlightBlock(block)
+      forEach(hlRef.current.querySelectorAll<HTMLElement>("pre > code"), (block) => {
+        hljs.highlightBlock(block)
+      })
     })
-  })
+  }, dependencies)
 
   return hlRef
 }

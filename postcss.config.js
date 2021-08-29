d63abce68d5794c2ca9c7745e69b70f81432eef1
Hugo Wallenburg <hugow@hey.com>
2021-08-29 16:57:46 +0200

    Fixes 100vh bug on iOS Safari

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,5 @@
 const tailwindcss = require("tailwindcss")
+const postcss100vhfix = require("postcss-100vh-fix")
 const autoprefixer = require("autoprefixer")
 const cssnano = require("cssnano")
 
@@ -7,7 +8,7 @@ const config = {
   plugins: [
     //Some plugins, like postcss-nested, need to run before Tailwind,
     tailwindcss(),
-    //But others, like autoprefixer, need to run after,
+    postcss100vhfix(),
     autoprefixer(),
     process.env.NODE_ENV === "production" && cssnano({
       preset: "default"

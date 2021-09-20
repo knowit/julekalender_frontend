fc87390d6391e8a55b523b1178faa104f4ed4925
Vegard Hesselberg <vegard.hesselberg@knowit.no>
2021-09-20 20:15:23 +0200

    Moved z-depth class out one component, added position:relative to said component.

--- src/components/Door/DoorBorder.tsx
+++ src/components/Door/DoorBorder.tsx
@@ -4,8 +4,8 @@ import { ReactComponent as Border } from "../svg/mistletoeborder.svg"
 
 
 const DoorBorder: FC = () => (
-  <div className="-mb-20 overflow-hidden flex justify-center">
-    <Border className="z-10 w-kodekalender flex-shrink-0" />
+  <div className="-mb-20 z-10 relative overflow-hidden flex justify-center">
+    <Border className="w-kodekalender flex-shrink-0" />
   </div>
 )
 

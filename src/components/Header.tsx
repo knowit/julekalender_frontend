d785f6300e8efb02177d7a3916035295a4e222ba
Hugo Wallenburg <hugow@hey.com>
2021-10-25 20:21:08 +0200

    Removes 'toggle admin' button. Feature is just hassle.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,8 @@
-import { FC, useCallback } from "react"
+import { FC } from "react"
 import { Link } from "react-router-dom"
 import { FaLock } from "react-icons/fa"
-import { useQueryClient } from "react-query"
-import { merge } from "lodash"
 
 import { ReactComponent as Logo } from "../img/knowitlogo.svg"
-import { Whoami } from "../api/User"
 import { useIsAdmin } from "../hooks/useIsAdmin"
 import { usePrefetchLeaderboard } from "../api/requests"
 
@@ -19,17 +16,7 @@ type HeaderProps = {
 
 const Header: FC<HeaderProps> = ({ setLeaderboardHidden }) => {
   const isAdmin = useIsAdmin()
-  const queryClient = useQueryClient()
   const prefetchLeaderboard = usePrefetchLeaderboard()
-
-  const isLocalhost = window.location.hostname === "localhost"
-
-  const toggleAdmin = useCallback(() => {
-    queryClient.setQueryData(
-      ["whoami"],
-      (whoami: Whoami | undefined) => merge(whoami, { is_admin: !isAdmin })
-    )
-  }, [queryClient, isAdmin])
 
   return (
     <header>
@@ -39,15 +26,11 @@ const Header: FC<HeaderProps> = ({ setLeaderboardHidden }) => {
         </a>
         <div className="float-right space-x-2 sm:space-x-6 h-10 mt-0.5 md:mt-1">
           {isAdmin && <>
-            <span className="text-green-500 uppercase tracking-wider hidden sm:inline">You are admin!</span>
             <Link to="/admin" title="Super secret admin pages">
               <Button className="hidden sm:inline">Admin page</Button>
               <Button className="sm:hidden"><FaLock /></Button>
             </Link>
           </>}
-
-          {isLocalhost && <Button className="hidden sm:inline" onClick={toggleAdmin}>Toggle admin</Button>}
-          {isLocalhost && <Button className="sm:hidden" onClick={toggleAdmin}>Admin</Button>}
 
           {/* Link to separate page on mobile */}
           <Button className="hidden sm:inline" onMouseEnter={prefetchLeaderboard} onClick={() => setLeaderboardHidden(false)} tabIndex={2}>Ledertavle</Button>

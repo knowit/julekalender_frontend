d7b965d24f27fe3db9aaa2a6eb64ca55ad44847c
Hugo Wallenburg <hugow@hey.com>
2021-12-01 10:04:54 +0100

    Fixes insane overflow in posts on long preformatted lines

--- src/components/Posts/PostsSection.tsx
+++ src/components/Posts/PostsSection.tsx
@@ -22,7 +22,13 @@ const PostsSection: FC<PostsSectionProps> = ({ door, usePosts = useUserPosts, wi
     <section className="mx-2 md:mx-8 lg:w-4/5 lg:mx-auto grid gap-door-elements justify-items-center">
       {!withoutInput && <PostForm door={door} />}
 
-      <div className="space-y-4 w-full">
+      {
+        /*
+        * Having min-w-0 (min-width: 0) prevents the content of the grid cells from growing outside of their cell:
+        * https://stackoverflow.com/questions/43311943/prevent-content-from-expanding-grid-items
+        */
+      }
+      <div className="space-y-4 w-full min-w-0">
         {map(posts, (post) =>
           <Post
             key={post.uuid}

7e341adf0fd44e86eee09ab383b5109eaba3b7d0
Hugo Wallenburg <hugow@hey.com>
2021-12-25 01:54:53 +0100

    Adds util to get raffle end time, shows message on main page when raffle is over

---
+++ src/hooks/useCurrentTime.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react"
+
+
+const useCurrentTime = () => {
+  const [date, setDate] = useState(new Date())
+
+  useEffect(() => {
+    const interval = setInterval(() => setDate(new Date()), 1000)
+    return () => clearInterval(interval)
+  }, [setDate])
+
+  return date
+}
+
+export default useCurrentTime

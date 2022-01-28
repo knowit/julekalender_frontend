2b89feade749f9042562e984ce085cd540433d55
Hugo Wallenburg <hugow@hey.com>
2022-01-28 11:47:19 +0100

    Adds notice for winner 2021

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,13 @@
 import { createRoot } from "react-dom"
-import { QueryClient, QueryClientProvider } from "react-query"
-import { BrowserRouter } from "react-router-dom"
-import { ReactQueryDevtools } from "react-query/devtools"
 
 import "../assets/css/base.scss"
-import "../assets/css/syntax_highlight.scss"
 
-import "./axios"
 import App from "./App"
-import AuthContext from "./AuthContext"
 
 
 const Main = () => {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        notifyOnChangeProps: "tracked"
-      }
-    }
-  })
-
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <AuthContext>
-          <ReactQueryDevtools />
-
-          <App />
-        </AuthContext>
-      </QueryClientProvider>
-    </BrowserRouter>
+    <App />
   )
 }
 
